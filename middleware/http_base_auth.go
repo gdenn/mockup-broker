50f7e2c9881268fbeec291dcbbb4abdaaf6b0a68
gdenn <gdennis91@googlemail.com>
2017-04-05 13:43:24 +0200

    Add http basic auth

--- middleware/http_base_auth.go
+++ middleware/http_base_auth.go
@@ -1,23 +1,40 @@
 package middleware
 
-import "net/http"
+import (
+	"encoding/base64"
+	"net/http"
+	"strings"
+)
 
-const validUserCredentials := make(map[string]string) {
-"name": "John",
-"password"
-}
+// Mockup user credentials
+var vc = map[string]string{"username": "John", "password": "test"}
 
 // Use HttpBaseAuth to authenticate user through middleware
 func HttpBaseAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !authenticate(r) {
-			w.WriteHeader(http.StatusUnauthorized)
+		authString := strings.SplitN(r.Header.Get("Authorization"), " ", 2)
+
+		if len(authString) != 2 || authString[0] != "Basic" {
+			http.Error(w, "authorization failed", http.StatusUnauthorized)
 			return
 		}
+
+		payload, _ := base64.StdEncoding.DecodeString(authString[1])
+		pair := strings.SplitN(string(payload), ":", 2)
+
+		if len(pair) != 2 || !validate(pair[0], pair[1]) {
+			http.Error(w, "authorization failed", http.StatusUnauthorized)
+			return
+		}
+
 		next.ServeHTTP(w, r)
 	})
 }
 
-func authenticate(r *http.Request) bool {
-	return true
+// Check for valid credentials
+func validate(username, password string) bool {
+	if vc["username"] == username && vc["password"] == password {
+		return true
+	}
+	return false
 }
